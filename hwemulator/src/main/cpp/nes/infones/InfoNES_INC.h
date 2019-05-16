03c4d860e855eee43b3089af5d4bc1bcdf443b39
李明艺 <lmyooyo@gmail.com>
2019-05-16 20:38:16 +0800

    集中mapper需要的头文件

---
+++ hwemulator/src/main/cpp/nes/infones/InfoNES_INC.h
@@ -0,0 +1,15 @@
+//
+// Created by mingyi.li on 2019/5/16.
+//
+
+#ifndef HWEMULATOR_INFONES_INC_H
+#define HWEMULATOR_INFONES_INC_H
+
+#include "InfoNES.h"
+#include "InfoNES_Mapper.h"
+#include "InfoNES_System.h"
+#include "K6502.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif //HWEMULATOR_INFONES_INC_H
