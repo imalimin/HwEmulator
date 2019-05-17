efc4b59460464e4955ce82d1af8c3102f98e9164
李明艺 <lmyooyo@gmail.com>
2019-05-17 14:47:58 +0800

    增加文件打开逻辑

--- app/src/main/java/com/lmy/sample/emulator/ui/MainActivity.kt
+++ app/src/main/java/com/lmy/sample/emulator/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.lmy.sample.emulator.ui
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.view.SurfaceHolder
 import android.view.View
+import android.widget.Toast
 import com.lmy.emulator.HwEmulator
 import com.lmy.sample.emulator.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,6 +23,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var uri = intent.data
+        if (uri == null)
+            uri = intent.getParcelableExtra(Intent.EXTRA_STREAM)
+        if (uri == null) {
+            val testFile = File(Environment.getExternalStorageDirectory(), "rxgd.nes")
+            if (!testFile.exists()) {
+                Toast.makeText(this, "没有找到该文件", Toast.LENGTH_SHORT).show()
+                finish()
+                return
+            }
+            uri = Uri.fromFile(testFile)
+        }
+        val path = getRealFilePath(uri)
+        if (TextUtils.isEmpty(path)) {
+            Toast.makeText(this, "没有找到该文件", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
         startBtn.setOnClickListener(this)
         surfaceView.keepScreenOn = true
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
@@ -28,7 +53,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             override fun surfaceCreated(holder: SurfaceHolder) {
-                mEmulator.prepare("${Environment.getExternalStorageDirectory().path}/rxgd.nes", holder.surface)
+                mEmulator.prepare(path!!, holder.surface)
                 thread = object : Thread() {
                     override fun run() {
                         mEmulator.start()
@@ -46,5 +71,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    private fun getRealFilePath(uri: Uri?): String? {
+        if (null == uri) return null
+        val scheme = uri.scheme
+        var data: String? = null
+        if (scheme == null)
+            data = uri.path
+        else if (ContentResolver.SCHEME_FILE == scheme) {
+            data = uri.path
+        } else if (ContentResolver.SCHEME_CONTENT == scheme) {
+            val cursor = contentResolver.query(uri, arrayOf(MediaStore.Images.ImageColumns.DATA), null, null, null)
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    val index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+                    if (index > -1) {
+                        data = cursor.getString(index)
+                    }
+                }
+                cursor.close()
+            }
+        }
+        return data
     }
 }
