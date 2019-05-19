eda0ee7fc49cf684adde85e599523bf863bb6904
limin <lmyooyo@gmail.com>
2019-05-20 00:27:56 +0800

    完善逻辑

--- hwemulator/src/main/java/com/lmy/emulator/widget/GamePadView.kt
+++ hwemulator/src/main/java/com/lmy/emulator/widget/GamePadView.kt
@@ -3,6 +3,7 @@ package com.lmy.emulator.widget
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import com.lmy.emulator.entity.HwGamePadEvent
@@ -13,8 +14,7 @@ open class GamePadView : View {
     private val buttonMap = HashMap<Int, HwRegion>()
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var listener: ((HwGamePadEvent) -> Unit)? = null
-    private var lastKey = HwGamePadEvent.KEY_NONE
-    private var lastMaskedKey = HwGamePadEvent.KEY_NONE
+    private var lastKeyMap = HashMap<Int, Int>()
 
     constructor(context: Context) : super(context) {
         applyStyle()
@@ -56,22 +56,28 @@ open class GamePadView : View {
                 MotionEvent.ACTION_POINTER_UP == event.actionMasked) {
             for (i in 0 until event.pointerCount) {
                 val id = event.getPointerId(i)
-                val x = event.getX(id).toInt()
-                val y = event.getY(id).toInt()
+                val x = event.getX(i).toInt()
+                val y = event.getY(i).toInt()
                 buttonMap.map {
                     if (it.value.contains(PointF(x.toFloat(), y.toFloat()))) {
                         if (MotionEvent.ACTION_DOWN == event.actionMasked ||
                                 MotionEvent.ACTION_MOVE == event.actionMasked ||
                                 MotionEvent.ACTION_UP == event.actionMasked) {
-                            if (lastKey != it.key) {
-                                listener?.invoke(HwGamePadEvent.create(lastKey, HwGamePadEvent.ACTION_UP))
+                            if (lastKeyMap.containsKey(it.key) && lastKeyMap[id] != it.key) {
+                                listener?.invoke(HwGamePadEvent.create(lastKeyMap[id]!!, HwGamePadEvent.ACTION_UP))
                             }
                             if (MotionEvent.ACTION_UP == event.actionMasked) {
                                 listener?.invoke(HwGamePadEvent.create(it.key, HwGamePadEvent.ACTION_UP))
                             } else {
                                 listener?.invoke(HwGamePadEvent.create(it.key, HwGamePadEvent.ACTION_DOWN))
                             }
-                            lastKey = it.key
+                            lastKeyMap[id] = it.key
+                        }
+                        if (MotionEvent.ACTION_UP == event.action) {
+                            lastKeyMap.map { item ->
+                                listener?.invoke(HwGamePadEvent.create(item.key, HwGamePadEvent.ACTION_UP))
+                            }
+                            lastKeyMap.clear()
                         }
                         return true
                     }
